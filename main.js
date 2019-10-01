6d889970b8532e42cd6212462a2514a9afd8b174
mi-ko-cute <thx-mail@worysolve.com>
2019-10-01 10:05:22 +0900

    エクササイズ１をこなしました

--- main.js
+++ main.js
@@ -3,31 +3,36 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const idTitle = document.getElementById('title');
+console.log(idTitle);
 
 /**
  * 課題2: id値が 'item-container' の要素を取得する
  *   - querySelectorを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const idItem_Container = document.querySelector('#item-container');
+console.log(idItem_Container);
 
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const li = document.getElementsByTagName('li');
+console.log(li);
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
  *   - getElementsByClassNameを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const classItem = document.getElementsByClassName('item');
+console.log(classItem);
 
 /**
  * 課題5: class値が 'item' の要素を全て取得する
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+const classItemAll = document.querySelectorAll('.item');
+console.log(classItemAll);
